a62c282a2fff3fc727dd0ca9d0de9f4acd925f24
Huytrong1512 <trongnhph32253@fpt.edu.vn>
2024-03-17 14:19:37 +0700

    new update

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        shop: resolve(__dirname, './shop.html'),
+        datail: resolve(__dirname, './datail-product.html'),
+        cart: resolve(__dirname, './cart.html'),
+        pay: resolve(__dirname, './pay-product.html'),
+        login: resolve(__dirname, './login.html'),
+      },
+    },
+  },
+})
